cfdf5e4d95d06c469551a07bfbeb69aecb30876c
Jozef S <franck.vukelic@gmail.com>
2024-07-23 21:45:39 +0700

    Update

--- client/src/client.js
+++ client/src/client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 import imageUrlbuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "s2ij3lfu",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
